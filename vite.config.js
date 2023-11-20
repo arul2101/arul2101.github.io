3e1fba09d6347bc1a7653b95e3626f358acb6894
arul2101 <myanuarullah20@gmail.com>
2023-11-20 18:55:46 +0700

    fix some issues

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path';
+import { URL } from 'url';
+
+const __dirname = new URL('.', import.meta.url).pathname;
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/my-portfolio/'
+  base: '/my-portfolio/',
+  build: {
+    outDir: path.resolve(__dirname, 'dist')
+  }
 })
