ea4386015bc6a8780f7899ff7bfc6f967ee0e473
arul2101 <myanuarullah20@gmail.com>
2024-01-20 13:56:12 +0700

    update some files

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -10,50 +10,50 @@ import { FiInstagram } from "react-icons/fi";
 import { FiLinkedin } from "react-icons/fi";
 import { NavLink } from 'react-router-dom';
 
-import MyCv from './../../data/cv-updated-2023.pdf';
+import MyCv from "./../../data/resume_updated_2024.pdf";
 
 export default function Hero() {
 
-  return (
-    <div className="hero">
-        <m.div
-            className="logo-sosmed"
-            initial={{ x: "-75%", opacity: 0 }}
-            animate={{ x: "0%", opacity: 1 }}
-            exit={{ opacity: 0 }}
-            transition={{ duration: .50, ease: "linear" }}
-        >
-            <a href='https://github.com/arul2101' target="_blank" className="icon" rel="noreferrer"><FiGithub /></a>
-            <a href='https://web.facebook.com/m.yanuarullah.assidiq' target="_blank" className="icon" rel="noreferrer"><FiFacebook /></a>
-            <a href='https://instagram.com/m.yanuarullah' target="_blank" className="icon" rel="noreferrer"><FiInstagram /></a>
-            <a href='https://www.linkedin.com/in/muhammad-yanuarullah-assidiq-90a698255/' target="_blank" className="icon" rel="noreferrer"><FiLinkedin /></a>
-        </m.div>
+    return (
+        <div className="hero">
+            <m.div
+                className="logo-sosmed"
+                initial={{ x: "-75%", opacity: 0 }}
+                animate={{ x: "0%", opacity: 1 }}
+                exit={{ opacity: 0 }}
+                transition={{ duration: .50, ease: "linear" }}
+            >
+                <a href='https://github.com/arul2101' target="_blank" className="icon" rel="noreferrer"><FiGithub /></a>
+                <a href='https://web.facebook.com/m.yanuarullah.assidiq' target="_blank" className="icon" rel="noreferrer"><FiFacebook /></a>
+                <a href='https://instagram.com/m.yanuarullah' target="_blank" className="icon" rel="noreferrer"><FiInstagram /></a>
+                <a href='https://www.linkedin.com/in/muhammad-yanuarullah-assidiq-90a698255/' target="_blank" className="icon" rel="noreferrer"><FiLinkedin /></a>
+            </m.div>
 
-        <m.div
-            className="hero-desc"
-            initial={{ x: "25%", opacity: 0 }}
-            animate={{ x: "0%", opacity: 1 }}
-            exit={{ opacity: 0 }}
-            transition={{ duration: .50, ease: "linear" }}
-        >
-            <h3>Sidiq Yanuar👋</h3>
-            <p className='hero-sub-title'>--- Web Development Enthusiast</p>
-            <p className='hero-sub-desc'>Im a simple, creative, enthusiastic and fun loving person. My dream is to create something for help many people..</p>
-            <div className="hero-btn-portfolio">
-                <NavLink to="portfolio" className="btn-portfolio">Portfolio</NavLink>
+            <m.div
+                className="hero-desc"
+                initial={{ x: "25%", opacity: 0 }}
+                animate={{ x: "0%", opacity: 1 }}
+                exit={{ opacity: 0 }}
+                transition={{ duration: .50, ease: "linear" }}
+            >
+                <h3>Sidiq Yanuar👋</h3>
+                <p className='hero-sub-title'>--- Web Development Enthusiast</p>
+                <p className='hero-sub-desc'>Im a simple, creative, enthusiastic and fun loving person. My dream is to create something for help many people..</p>
+                <div className="hero-btn-portfolio">
+                    <NavLink to="portfolio" className="btn-portfolio">Portfolio</NavLink>
 
-                <a
-                    href={MyCv}
-                    className="resume"
-                    target="_blank" 
-                    rel="noreferrer"
-                    download="resume-updated-2023.pdf"
-                ><FiDownload /> Download Resume</a>
+                    <a
+                        href={MyCv}
+                        className="resume"
+                        target="_blank"
+                        rel="noreferrer"
+                        download="Arul's Resume 2024.pdf"
+                    ><FiDownload /> Download Resume</a>
+                </div>
+            </m.div>
+
+            <div className="profile-picture">
             </div>
-        </m.div>
-
-        <div className="profile-picture">
         </div>
-    </div>
-  )
+    )
 }
--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -22,11 +22,11 @@ export default function About() {
         transition={{ duration: .75, ease: "linear" }}
       >About Me</m.h3>
       <m.p
-          className="desc-about"
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          exit={{ opacity: 0 }}
-          transition={{ duration: .75, ease: "linear" }}
+        className="desc-about"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        transition={{ duration: .75, ease: "linear" }}
       >My Introduction</m.p>
 
       <div className="row-about">
@@ -40,14 +40,14 @@ export default function About() {
           <div className="image"></div>
         </m.div>
 
-        <m.div 
+        <m.div
           className="about-description"
           initial={{ x: "25%", opacity: 0 }}
           animate={{ x: "0%", opacity: 1 }}
           exit={{ opacity: 0 }}
           transition={{ duration: .75, ease: "linear" }}
         >
-          <p><span>Hello!</span> My full name is <span>Muhammad Yanuarullah Assidiq</span>, you can call me <span>Arul</span> or <span>Anwar</span>. Im 22 years old, I was born on January 21, 2001 in <span>Indonesia</span>, Jakarta. </p><br />
+          <p><span>Hello!</span> My full name is <span>Muhammad Yanuarullah Assidiq</span>, you can call me <span>Arul</span> or <span>Anwar</span>. Im 23 years old, I was born on January 21, 2001 in <span>Indonesia</span>, Jakarta. </p><br />
           <p>Im obsessed with making things and even more obsessed with making things <span>better</span>. In everyday life, I work as a <span>data entry</span> in a company in Jakarta. I have been interested in <span>web development</span> since 2018 and started <span>self-taught</span> since 2019.</p><br />
 
           <p>If you interested about my work experience, you can visit my LinkedIn</p><br />
